c29e9c3b2842daba711886d6d11411fa6dd99e6b
davidek-ui <chabera.david@gmail.com>
2024-03-07 23:16:09 +0100

    commit

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -15,8 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Strongest'),
-    );
+      home: const MyHomePage(title: 'Profile'));
   }
 }
 
@@ -30,13 +29,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+
+  
 
   @override
   Widget build(BuildContext context) {
@@ -46,29 +41,15 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline6,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      
+      
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.purple,
         currentIndex: 0,
         onTap: (index) {
+          if (index == 0) { // Check if the tapped index is 0 (home page)
+            Navigator.popUntil(context, ModalRoute.withName('/'));
+            }
           if (index == 1) {
             Navigator.push(
               context,
@@ -79,6 +60,18 @@ class _MyHomePageState extends State<MyHomePage> {
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => const  ThirdScreen()),
+            );
+          }
+          if (index == 3) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const  FourthScreen()),
+            );
+          }
+          if (index == 4) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const  FifthScreen()),
             );
           }
         },
@@ -116,7 +109,10 @@ class SecondScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.purple,
-        title: const Text('Second Screen'),
+        title: const Text(
+          'Random Generator',
+          style: TextStyle(color: Colors.white, fontSize: 30),
+          ),
       ),
       backgroundColor: Colors.black,
       body: Center(
@@ -159,10 +155,28 @@ class SecondScreen extends StatelessWidget {
           if (index == 0) { // Check if the tapped index is 0 (home page)
             Navigator.popUntil(context, ModalRoute.withName('/'));
             }
+          if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondScreen()),
+            );
+          }
           if (index == 2) {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => const ThirdScreen()),
+              MaterialPageRoute(builder: (context) => const  ThirdScreen()),
+            );
+          }
+          if (index == 3) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const  FourthScreen()),
+            );
+          }
+          if (index == 4) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const  FifthScreen()),
             );
           }
         },
@@ -195,15 +209,34 @@ class SecondScreen extends StatelessWidget {
 }
 
 class ThirdScreen extends StatelessWidget {
+
+  
+
   const ThirdScreen({Key? key}) : super(key: key);
   @override
+  
   Widget build(BuildContext context) {
+    void plustemplate() {
+    Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SixthScreen()),
+            );
+  }
     return Scaffold(
       
       appBar: AppBar(
-        title: const Text('Second Screen'),
+        title: const Text(
+          'Templates',
+          style: TextStyle(color: Colors.white, fontSize: 30),
+          ),
         backgroundColor: Colors.purple,
       
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: plustemplate,
+        tooltip: 'Plus template',
+        child: const Icon(Icons.add, ),
+        backgroundColor: Colors.purple,
       ),
       backgroundColor: Colors.black,
       body: Center(
@@ -261,6 +294,24 @@ class ThirdScreen extends StatelessWidget {
               MaterialPageRoute(builder: (context) => const SecondScreen()),
             );
           }
+          if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const  ThirdScreen()),
+            );
+          }
+          if (index == 3) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const  FourthScreen()),
+            );
+          }
+          if (index == 4) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const  FifthScreen()),
+            );
+          }
         },
         items: const [
           BottomNavigationBarItem(
@@ -288,3 +339,325 @@ class ThirdScreen extends StatelessWidget {
     );
   }
 }
+
+
+class FourthScreen extends StatelessWidget {
+  const FourthScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      appBar: AppBar(
+        title: const Text(
+          'Exercises',
+          style: TextStyle(color: Colors.white, fontSize: 30),
+          ),
+        backgroundColor: Colors.purple,
+      
+      ),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.grey,
+              ),
+              onPressed: () {
+
+              }, 
+              child: const Text('Tlačítko na '),
+              ),
+            const Text(
+              'This is the third screen.',
+              style: TextStyle(fontSize: 24),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text('Custom Dialog'),
+                      content: const Text('halooo'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('Close'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: const  Text('Custom Button'),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.purple,
+        currentIndex: 3,
+        onTap: (index) {
+          if (index == 0) { // Check if the tapped index is 0 (home page)
+            Navigator.popUntil(context, ModalRoute.withName('/'));
+            }
+          if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondScreen()),
+            );
+          }
+          if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const  ThirdScreen()),
+            );
+          }
+          if (index == 3) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const  FourthScreen()),
+            );
+          }
+          if (index == 4) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const  FifthScreen()),
+            );
+          }
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.casino),
+            label: 'Random',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Templates',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fitness_center),
+            label: 'Exercises',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.alarm),
+            label: 'History',
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class FifthScreen extends StatelessWidget {
+  const FifthScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      appBar: AppBar(
+        title: const Text(
+          'History',
+          style: TextStyle(color: Colors.white, fontSize: 30),
+        ),
+  
+        backgroundColor: Colors.purple,
+      
+      ),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text('Custom Dialog'),
+                      content: const Text('halooo'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('Close'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: const  Text('Custom Button'),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.purple,
+        currentIndex: 4,
+        onTap: (index) {
+          if (index == 0) { // Check if the tapped index is 0 (home page)
+            Navigator.popUntil(context, ModalRoute.withName('/'));
+            }
+          if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondScreen()),
+            );
+          }
+          if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const  ThirdScreen()),
+            );
+          }
+          if (index == 3) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const  FourthScreen()),
+            );
+          }
+          if (index == 4) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const  FifthScreen()),
+            );
+          }
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.casino),
+            label: 'Random',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Templates',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fitness_center),
+            label: 'Exercises',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.alarm),
+            label: 'History',
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SixthScreen extends StatelessWidget {
+  const SixthScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      appBar: AppBar(
+        title: const Text('Second Screen'),
+        backgroundColor: Colors.purple,
+      
+      ),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[  
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text('Custom Dialog'),
+                      content: const Text('halooo'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('Close'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: const  Text('Custom Button'),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.purple,
+        currentIndex: 2,
+        onTap: (index) {
+          if (index == 0) { // Check if the tapped index is 0 (home page)
+            Navigator.popUntil(context, ModalRoute.withName('/'));
+            }
+          if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondScreen()),
+            );
+          }
+          if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const  ThirdScreen()),
+            );
+          }
+          if (index == 3) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const  FourthScreen()),
+            );
+          }
+          if (index == 4) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const  FifthScreen()),
+            );
+          }
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.casino),
+            label: 'Random',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Templates',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fitness_center),
+            label: 'Exercises',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.alarm),
+            label: 'History',
+          )
+        ],
+      ),
+    );
+  }
+}
+

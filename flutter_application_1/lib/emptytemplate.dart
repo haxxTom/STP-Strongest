9a49054bd08d037f4b11981fff64b6b2d5cbedeb
Tomas Kapoun <tomkapoun679@gmail.com>
2025-02-12 14:16:26 +0100

    template

--- flutter_application_1/lib/emptytemplate.dart
+++ flutter_application_1/lib/emptytemplate.dart
@@ -1,62 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart'; // For FilteringTextInputFormatter
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'database/exercisesList.dart';
 import 'database/database.dart';
 import 'templates.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:convert';
 
-class NewTemplateScreen extends StatefulWidget {
-  final Trenink trenink;
-
-  NewTemplateScreen({required this.trenink});
-
+class TemplateWorkoutScreen extends StatefulWidget {
   @override
-  _NewTemplateScreenState createState() => _NewTemplateScreenState();
+  _TemplateWorkoutScreenState createState() => _TemplateWorkoutScreenState();
 }
 
-class _NewTemplateScreenState extends State<NewTemplateScreen> {
-  Duration _elapsedTime = Duration();
+class _TemplateWorkoutScreenState extends State<TemplateWorkoutScreen> {
+  List<Map<String, dynamic>> _templates = [];
   TextEditingController _titleController = TextEditingController();
-  bool _isEditingTitle = false;
-
-  Map<int, List<Map<String, TextEditingController>>> setControllers = {};
+  List<Cvik> _exercises = [];
 
   @override
   void initState() {
     super.initState();
-    _titleController.text = widget.trenink.nazev;
+    _loadTemplates();
   }
 
-  void _toggleTitleEdit() {
+  Future<void> _loadTemplates() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? templatesJson = prefs.getString('workout_templates');
+    if (templatesJson != null) {
+      setState(() {
+        _templates = List<Map<String, dynamic>>.from(jsonDecode(templatesJson));
+      });
+    }
+  }
+
+  Future<void> _saveTemplate() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    Map<String, dynamic> template = {
+      'name': _titleController.text,
+      'exercises': _exercises.map((e) => e.toJson()).toList(),
+    };
+    _templates.add(template);
+    await prefs.setString('workout_templates', jsonEncode(_templates));
+    setState(() {});
+  }
+
+  void _deleteTemplate(int index) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
-      _isEditingTitle = !_isEditingTitle;
+      _templates.removeAt(index);
+    });
+    await prefs.setString('workout_templates', jsonEncode(_templates));
+  }
+
+  void _addExercise(Cvik cvik) {
+    setState(() {
+      _exercises.add(cvik);
     });
   }
 
-  void _addExerciseDialog(BuildContext context) {
+  void _showExerciseSelectionDialog() {
     showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: const Text("Vyberte cvik"),
-          content: SizedBox(
+          title: Text("Vyberte cvik"),
+          content: Container(
             height: 300,
-            width: 300,
             child: ListView.builder(
-              itemCount: cviky.length,
+              itemCount: scviky.length,
               itemBuilder: (context, index) {
-                final cvik = cviky[index];
                 return ListTile(
-                  leading: Image.asset(cvik.obrazek),
-                  title: Text(cvik.nazev),
-                  subtitle: Text(cvik.partie),
+                  title: Text(scviky[index].nazev),
                   onTap: () {
-                    setState(() {
-                      addExerciseFromList(widget.trenink, cvik.id);
-                      int exerciseIndex = widget.trenink.exercises.length - 1;
-                      _addSet(exerciseIndex);
-                    });
+                    _addExercise(scviky[index]);
                     Navigator.pop(context);
                   },
                 );
@@ -68,281 +82,62 @@ class _NewTemplateScreenState extends State<NewTemplateScreen> {
     );
   }
 
-  void _addSet(int exerciseIndex) {
-    setState(() {
-      if (!setControllers.containsKey(exerciseIndex)) {
-        setControllers[exerciseIndex] = [];
-      }
-
-      var weightController = TextEditingController();
-      var repsController = TextEditingController();
-
-      setControllers[exerciseIndex]!.add({
-        'weight': weightController,
-        'reps': repsController,
-      });
-    });
-  }
-
-  void _deleteSet(int exerciseIndex, int setIndex) {
-    setState(() {
-      setControllers[exerciseIndex]!.removeAt(setIndex);
-    });
-  }
-
-  void _deleteExercise(int exerciseIndex) {
-    setState(() {
-      widget.trenink.exercises.removeAt(exerciseIndex);
-      setControllers.remove(exerciseIndex);
-    });
-  }
-
-  List<Map<String, String>> _getSetsData() {
-    List<Map<String, String>> allSetsData = [];
-    setControllers.forEach((exerciseIndex, sets) {
-      sets.forEach((setController) {
-        allSetsData.add({
-          'weight': setController['weight']!.text,
-          'reps': setController['reps']!.text,
-        });
-      });
-    });
-    return allSetsData;
-  }
-
-  bool _validateSetsData() {
-    bool isValid = true;
-    setControllers.forEach((exerciseIndex, sets) {
-      for (var setController in sets) {
-        final weightText = setController['weight']?.text ?? '';
-        final repsText = setController['reps']?.text ?? '';
-        
-        if (weightText.isEmpty || repsText.isEmpty) {
-          isValid = false;
-          break;
-        }
-      }
-    });
-    return isValid;
-  }
-
-  Future<void> _saveTemplate() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String templateJson = jsonEncode(widget.trenink.toJson());
-    await prefs.setString('activeTemplate', templateJson);
-  }
-
-  Future<void> _saveSetsData() async {
-    final prefs = await SharedPreferences.getInstance();
-    List<Map<String, String>> allSetsData = _getSetsData();
-
-    String allSetsDataString = allSetsData
-        .map((setData) => '{"weight": "${setData['weight']}", "reps": "${setData['reps']}"}')
-        .join(',');
-
-    await prefs.setString('setsData', allSetsDataString);
-    print('Data byla uložena.');
-  }
-
-  Future<void> _loadSetsData() async {
-    final prefs = await SharedPreferences.getInstance();
-    String? savedSetsDataString = prefs.getString('setsData');
-
-    if (savedSetsDataString != null) {
-      List<Map<String, String>> loadedSetsData = [];
-      List<String> sets = savedSetsDataString.split(',');
-
-      for (var set in sets) {
-        var weight = RegExp(r'"weight": "(.*?)"').firstMatch(set)?.group(1);
-        var reps = RegExp(r'"reps": "(.*?)"').firstMatch(set)?.group(1);
-        if (weight != null && reps != null) {
-          loadedSetsData.add({'weight': weight, 'reps': reps});
-        }
-      }
-
-      setState(() {
-        setControllers.clear();
-        int index = 0;
-        for (var setData in loadedSetsData) {
-          _addSet(index);
-          var lastSet = setControllers[index]?.last;
-          if (lastSet != null) {
-            lastSet['weight']?.text = setData['weight']!;
-            lastSet['reps']?.text = setData['reps']!;
-          }
-          index++;
-        }
-      });
-      print('Data byla načtena.');
-    }
-  }
-
-  void _finishTemplate() {
-    if (!_validateSetsData()) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Vyplňte všechny váhy a opakování!")),
-      );
-      return;
-    }
-
-    List<Map<String, String>> allSetsData = _getSetsData();
-    print('Uloženo ${allSetsData.length} sérií do databáze nebo souboru.');
-
-    // Po dokončení šablony navigujeme zpět na TemplateScreen
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => TemplateScreen()),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        _saveTemplate();
-        return true;
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Row(
-            children: [
-              _isEditingTitle
-                  ? Expanded(
-                      child: TextField(
-                        controller: _titleController,
-                        onSubmitted: (_) {
-                          setState(() {
-                            widget.trenink.nazev = _titleController.text;
-                            _toggleTitleEdit();
-                          });
-                        },
-                        autofocus: true,
-                      ),
-                    )
-                  : Expanded(
-                      child: GestureDetector(
-                        onTap: _toggleTitleEdit,
-                        child: Text(
-                          widget.trenink.nazev,
-                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
-              Text(
-                '${_elapsedTime.inMinutes}:${(_elapsedTime.inSeconds % 60).toString().padLeft(2, '0')}',
-                style: TextStyle(fontSize: 20),
+    return Scaffold(
+      appBar: AppBar(title: Text("Šablony tréninků"), backgroundColor: Colors.blueGrey),
+      backgroundColor: Colors.grey[900],
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _titleController,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                labelText: "Název tréninku",
+                labelStyle: TextStyle(color: Colors.white70),
+                filled: true,
+                fillColor: Colors.grey[800],
+                border: OutlineInputBorder(),
               ),
-            ],
+            ),
           ),
-        ),
-        backgroundColor: Colors.white,
-        body: ListView(
-          children: [
-            ...widget.trenink.exercises.map((exercise) {
-              int exerciseIndex = widget.trenink.exercises.indexOf(exercise);
-              return Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Card(
-                  elevation: 4,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Image.asset(
-                              exercise.cvik.obrazek,
-                              width: 50,
-                              height: 50,
-                            ),
-                            const SizedBox(width: 10),
-                            Text(
-                              exercise.cvik.nazev,
-                              style: const TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            IconButton(
-                              icon: const Icon(Icons.delete, color: Colors.red),
-                              onPressed: () {
-                                _deleteExercise(exerciseIndex);
-                              },
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 10),
-                        ListView.builder(
-                          shrinkWrap: true,
-                          physics: NeverScrollableScrollPhysics(),
-                          itemCount: setControllers[exerciseIndex]?.length ?? 0,
-                          itemBuilder: (context, setIndex) {
-                            var controllers = setControllers[exerciseIndex]![setIndex];
-                            return Row(
-                              children: [
-                                Text(
-                                  'Série ${setIndex + 1}:',
-                                  style: const TextStyle(fontSize: 16),
-                                ),
-                                Expanded(
-                                  child: TextField(
-                                    controller: controllers['weight'],
-                                    keyboardType: TextInputType.number,
-                                    inputFormatters: [
-                                      FilteringTextInputFormatter.digitsOnly,
-                                    ],
-                                    decoration: const InputDecoration(
-                                      labelText: "Váha",
-                                    ),
-                                  ),
-                                ),
-                                Expanded(
-                                  child: TextField(
-                                    controller: controllers['reps'],
-                                    keyboardType: TextInputType.number,
-                                    inputFormatters: [
-                                      FilteringTextInputFormatter.digitsOnly,
-                                    ],
-                                    decoration: const InputDecoration(
-                                      labelText: "Počet opakování",
-                                    ),
-                                  ),
-                                ),
-                                IconButton(
-                                  icon: const Icon(Icons.delete, color: Colors.red),
-                                  onPressed: () {
-                                    _deleteSet(exerciseIndex, setIndex);
-                                  },
-                                ),
-                              ],
-                            );
-                          },
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.add),
-                          onPressed: () {
-                            _addSet(exerciseIndex);
-                          },
-                        ),
-                      ],
+          Expanded(
+            child: ListView.builder(
+              itemCount: _exercises.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  color: Colors.grey[850],
+                  child: ListTile(
+                    title: Text(_exercises[index].nazev, style: TextStyle(color: Colors.white)),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete, color: Colors.red),
+                      onPressed: () {
+                        setState(() {
+                          _exercises.removeAt(index);
+                        });
+                      },
                     ),
                   ),
-                ),
-              );
-            }).toList(),
-            IconButton(
-              icon: const Icon(Icons.add_circle_outline),
-              onPressed: () {
-                _addExerciseDialog(context);
+                );
               },
             ),
-            ElevatedButton(
-              onPressed: _finishTemplate,
-              child: const Text("Dokončit šablonu"),
+          ),
+          ElevatedButton(
+            onPressed: _showExerciseSelectionDialog,
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+            child: Text("Přidat cvik", style: TextStyle(color: Colors.white)),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: _saveTemplate,
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.blueGrey),
+              child: Text("Uložit jako šablonu", style: TextStyle(color: Colors.white)),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
